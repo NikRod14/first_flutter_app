48082ec882a355e6d6702661d24f5725ba2b2745
We3NCJ <n_m_rodriguez@yahoo.com>
2025-05-05 22:30:15 -0400

    centering the UI

--- lib/main.dart
+++ lib/main.dart
@@ -39,20 +39,20 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.Current;
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
-        children: [
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [ 
           Text('A random idea:'),
-          Text(appState.current.asLowerCase),
           BigCard(pair: pair),
           ElevatedButton(
             onPressed: () {
               appState.getNext();
             },
             child: Text('Next'),
-          )
+          ),
         ],
       ),
     );
@@ -70,11 +70,14 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
